76287dceec45884960d2a0876b246a343def23da
WuDing <44403890+KingWuDing@users.noreply.github.com>
2018-11-20 17:51:16 +0800

    Add files via upload

---
+++ new 1.txt.js
@@ -0,0 +1,79 @@
+﻿<!DOCTYPE  html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> javascript 文件集</title>
+</head>
+
+<body>
+
+<title>......</title>
+
+<script>
+		function displayDate(){
+			document.getElementById("demo").innerHTML=Date();
+		}
+
+	</script>
+
+	<h1>我的第一个javascript程序</h1>
+	<p id="demo">北京时间</p>
+	<button type="button" onclick="displayDate()"> 显示日期 </button>
+
+	if isNaN(x){
+		alert("no number");
+	}
+
+function changeImage(){
+	var s =document.getElementById("myimage");
+	s.src=s.src.match("bulboff")?".....":"......";
+}
+
+
+window.alert()
+document.write()
+innerHTML
+console.log()
+
+var cars=new Array();
+cars[0]="Saab";
+cars[1]="Volvo";
+cars[2]="BMW";
+
+if(Array.isArray){
+	if(Array.isArray(cars)){
+		document.write("该对象是一个数组"）；
+	}
+}
+
+if (cars instanceof Array){
+		document.write("该对象是一个数组");
+}
+
+document.getElementById("demo").innerHTML="你好";
+
+var people={name:"Tom,age:22,eat: function(){  } }
+
+var people=new object();
+people.name='Tom';
+people.age=21;
+people.eat=function(){ }
+
+var d=new Date().getDay();
+switch(d)
+{
+	case 0:x="今天是星期日"；
+	break;
+
+<body>
+
+<script>
+	
+cars = array{"BMW","Volvo","Saab","Ford" };
+for (var i=0;i<cars.length;i++){
+document.write(cars[i] + "<br>"}
+};
+
+</script>
+
+<p> JS中，数组是一种特殊的对象。</p>
